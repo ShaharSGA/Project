6d964aab06817a66a853a7e23759af0ba85e8733
Shahar <Shahargurari@gmail.com>
2025-12-29 18:01:51 +0200

    Revert "Fix ImportError on Handshake page by removing eager imports from core/__init__.py"
    This reverts commit 25850be4a2d521743173de935de9ceb56b397709.

--- core/__init__.py
+++ core/__init__.py
@@ -1,14 +1,10 @@
 # Core module for Dana's Brain
-# Note: Imports are intentionally not done here to avoid heavy dependency loading
-# Import directly from submodules when needed:
-#   from core.crew_executor import execute_crew_async, CrewExecutionResult
-#   from core.file_manager import save_markdown_output
+
+from core.crew_executor import execute_crew_async, CrewExecutionResult
+from core.file_manager import save_markdown_output
 
 __all__ = [
-    'crew_executor',
-    'file_manager',
-    'auth',
-    'state_manager',
-    'content_parser',
-    'feedback_manager',
+    'execute_crew_async',
+    'CrewExecutionResult',
+    'save_markdown_output',
 ]
